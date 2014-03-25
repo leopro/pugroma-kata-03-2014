6271480582741d05d19020fc4861a8d7f6321842
Apolide <iulanto@gmail.com>
2014-03-25 20:07:17 +0100

    PugKata VINCENTE

--- src/PUGRoma/Kata/Kata.php
+++ src/PUGRoma/Kata/Kata.php
@@ -8,6 +8,37 @@ class Kata
     {
         return true;
     }
+
+    public function returnFizzBuzz($int){
+        $r="";
+        
+        $fizz = $int %3 === 0;
+        $buzz = $int %5 === 0;
+        
+        if($fizz AND $buzz){
+            return "FizzBuzz?";
+        }
+        
+        if($fizz){
+            return "Fizz";
+        }
+        
+        if($buzz){
+            return "Buzz";
+        }
+        
+        if($r===""){
+           return $int;
+        }
+        
+    }
+    
+    public function printFizzBuzz($from, $to)
+    {
+        for($n=$from; $n<=$to; $n++){
+            echo $this->returnFizzBuzz($n);
+        }
+    }
 }
 
 
--- src/PUGRoma/Tests/KataTest.php
+++ src/PUGRoma/Tests/KataTest.php
@@ -9,4 +9,29 @@ class KataTest extends \PHPUnit_Framework_TestCase
         $kata = new Kata();
         $this->assertTrue($kata->isReady());
     }
+
+    public function testTre()
+    {
+        $kata = new Kata();
+        $this->assertEquals("Fizz", $kata->returnFizzBuzz(3));
+    }
+    
+    public function testCinque()
+    {
+        $kata = new Kata();
+        $this->assertEquals("Buzz", $kata->returnFizzBuzz(5));
+    }
+
+    public function testTreCinque()
+    {
+        $kata = new Kata();
+        $this->assertEquals("FizzBuzz?", $kata->returnFizzBuzz(15));
+    }
+
+    public function testSette()
+    {
+        $kata = new Kata();
+        $this->assertEquals("7", $kata->returnFizzBuzz(7));
+    }
+    
 }
